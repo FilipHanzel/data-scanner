70a0dc25b570192f70ee4e098feefc2ed09917e4
Filip <filip.han@interia.pl>
2022-04-15 21:20:52 +0200

    added json tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -32,3 +32,64 @@ class TestJSONScannerRun(unittest.TestCase):
                 }
             ],
         )
+
+    def test_header_only_trailing_whitespace(self):
+        processor = Processor(
+            os.path.join(self.data_path, "single_record_header_only_trailing_whitespace.json"), "json"
+        )
+        schemas = processor.run()
+        self.assertEqual(
+            schemas,
+            [
+                {
+                    "this": "unknown",
+                    "file": "unknown",
+                    "has": "unknown",
+                    "only": "unknown",
+                    "nulls": "unknown",
+                }
+            ],
+        )
+
+    def test_valid_json_list(self):
+        processor = Processor(
+            os.path.join(self.data_path, "valid_json_list.json"), "json"
+        )
+        schemas = processor.run()
+        print(schemas)
+        self.assertEqual(
+            schemas,
+            [
+                {
+                    "a": "string",
+                    "c": "unknown", 
+                    "c_f": "json",
+                    "c_g": "string",
+                    "d": "json",
+                    "e": "json",
+                    "f": "integer",
+                    "g": "integer",
+                    "h": "float",
+                }
+            ],
+        )
+
+    def test_valid_json_single_record(self):
+        processor = Processor(
+            os.path.join(self.data_path, "valid_json_single_record.json"), "json"
+        )
+        schemas = processor.run()
+        self.assertEqual(
+            schemas,
+            [
+                {
+                    "a": "string",
+                    "c_f": "json",
+                    "c_g": "string",
+                    "d": "unknown",
+                    "f": "integer",
+                    "g": "integer",
+                    "h": "float",
+                }
+            ],
+        )
